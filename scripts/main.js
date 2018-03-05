a2f5875981b3924b9e36fca1aec914253a2a5b50
boboidream <z644381492@gmail.com>
2018-03-05 21:26:53 +0800

    v1

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+particlesJS.load("particles-js",'/scripts/snowflakes.json')
+
+$('h1').on('mouseenter', function () {
+    var $links = $('.links'),
+        $contact = $('.contact')
+    
+    $links.hide()
+    $contact.fadeIn()
+  })
+
+$('.info-wrapper').on('mouseleave', function () {
+    var $links = $('.links'),
+        $contact = $('.contact')
+    
+    $contact.hide()
+    $links.fadeIn()
+  })
